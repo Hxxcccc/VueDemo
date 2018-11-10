2c189ae3f9c51c72fa2e7c80054c6017485a177e
Hxc <hxc19980624@163.com>
2018-11-10 20:10:11 +0800

    保存todos

---
+++ src-01-base/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>4AMGodVzzZ wlswlswls</h2>
+    <img src="./assets/logo.png" alt="logo">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+
+  export default {
+    component: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style scoped>
+  img {
+    width: 100px;
+    height: 100px;
+  }
+</style>
---
+++ src-01-base/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: '人生有梦 各自精彩'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h2 {
+    color: deeppink;
+  }
+</style>
