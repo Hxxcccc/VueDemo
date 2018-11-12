b89d6308abaf3621a4d7b377a6c6826675520c57
Hxc <hxc19980624@163.com>
2018-11-12 11:56:16 +0800

    Mint Ui

---
+++ src-05-users/main.js
@@ -0,0 +1,16 @@
+/*
+  入口js
+ */
+
+import Vue from 'vue'
+import App from './App'
+
+new Vue ({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>'
+})
+
+
--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@
  */
 
 import Vue from 'vue'
+import {Button} from 'mint-ui'
+
 import App from './App'
+
+//全局注册组件 ==> 所有组件中都可以直接使用这个组件标签
+//Vue.component('mt-button', Button)
+Vue.component(Button.name, Button)
 
 new Vue ({
   el: '#app',
